cd1e532fb7387c6c1ad0e26f8ad2868fdbef8012
COITSILLE <losith22222@gmail.com>
2026-03-21 15:42:27 +0800

    reroll

--- Core/app/mechan_arm/arm_3DOF.h
+++
@@ -1,6 +0,0 @@
-#ifndef APP_ARM_3DOF_H
-#define APP_ARM_3DOF_H
-
-
-
-#endif /* APP_ARM_3DOF_H */
--- Core/app/slibs/smathex_invkine.c
+++
@@ -1,9 +0,0 @@
-#include "smathex_invkine.h"
-
-ik_Status_t ik_3DOF_checkReach(Vec3_t target, ik_3DOF_Params_t* params){
-    //TODO
-}
-
-Vec3_t ik_3DOF(Vec3_t target, ik_3DOF_Params_t params){
-    //TODO
-}
