9fc806751492b893ad82399161c898d467830495
COITSILLE <losith22222@gmail.com>
2026-03-23 23:29:58 +0800

    实现ik_3DOF_get

--- Software/servo/Core/app/slibs/smathex_invkine.h
+++ Software/servo/Core/app/slibs/smathex_invkine.h
@@ -9,6 +9,7 @@ typedef struct{
     float l1;
     float l2;
     float l3;
+    //TODO 
 } ik_3DOF_Params_t;
 
 typedef enum{
@@ -21,8 +22,8 @@ typedef enum{
     IK_UNREACHABLE,
 } ik_Status_t;
 
-ik_Status_t ik_3DOF_checkReach(Vec3_t target, ik_3DOF_Params_t* params);
-Vec3_t ik_3DOF_get(Vec3_t target, ik_3DOF_Params_t* params, ik_3DOF_RootConfig_t root_config);
+ik_Status_t ik_3DOF_checkReach(Vec3_t target, ik_3DOF_Params_t params);
+Vec3_t ik_3DOF_get(Vec3_t target, ik_3DOF_Params_t params, ik_3DOF_RootConfig_t root_config);
     //TODO;
 
 
