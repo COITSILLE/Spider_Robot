81aa1e5335cc58eed95497dca351b279b92eab9f
COITSILLE <losith22222@gmail.com>
2026-03-26 22:27:23 +0800

    文件位置调整

--- Software/servo/Core/app/pwm/pwm_pca9685.h
+++ Software/servo/Core/app/pwm/pwm_pca9685.h
@@ -2,6 +2,7 @@
 #define APP_CPP_PWM_PWM_PCA9685_H
 
 #include "pwm.h"
+#include "stm32f1xx_hal.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,12 +12,25 @@ extern "C" {
 
 class PWM_PCA9685 : public PWM<PWM_PCA9685> {
 public:
-    PWM_PCA9685();
-    void init(/*TODO*/);
+    PWM_PCA9685(I2C_HandleTypeDef *i2c,uint8_t addr, uint8_t channel, float osc_freq = 25000000.0f);
+    void init(float pwm_freq);
     void setDuty(float duty);
     void setTime(float time_ms);
-};
 
+private:
+    I2C_HandleTypeDef *hi2c_;
+    uint8_t dev_addr_;
+    uint8_t channel_;
+    float osc_freq_;
+    float pwm_freq_;
+    uint8_t pre_scale_;
+
+   static bool global_initialized_;
+
+   static bool writeReg(I2C_HandleTypeDef *hi2c, uint8_t addr, uint8_t reg, uint8_t val);
+   static bool writeRegs(I2C_HandleTypeDef *hi2c, uint8_t addr, uint8_t reg, uint8_t *data, uint8_t len);
+   static bool readReg(I2C_HandleTypeDef *hi2c, uint8_t addr, uint8_t reg, uint8_t val);
+   }
 #endif /* __cplusplus */
 
 #endif /* APP_CPP_PWM_PWM_PCA9685_H */
